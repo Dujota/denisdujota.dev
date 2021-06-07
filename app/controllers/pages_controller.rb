d3793e9cb1b0f4b2b8110df60b0858640be28449
Denis <ddujota@internationalsafety.com>
2021-06-07 01:05:17 -0500

    add contact page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def main
   end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root "pages#main"
+
+  # Pages
+  get "contact", to: "pages#contact"
 end
