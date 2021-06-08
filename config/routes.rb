8563d007c617f734d80ea9d552538ef9c0dc2231
Denis <ddujota@internationalsafety.com>
2021-06-08 15:27:34 -0500

    added the namsespacing for the posts routes to admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: [:registrations]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  root "pages#main"
-
-  # Pages
-  get "contact", to: "pages#contact"
+  scope :admin do
+    resources :posts
+  end
 
   # Blog Posts
-  resources :posts do
-  end
+  resources :posts, only: %i[index show]
+
+  # Pages
+  root "pages#main"
+  get "contact", to: "pages#contact"
 end
