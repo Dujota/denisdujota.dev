8a1c5a5ad05a1c0d234f44523894ab4018431ab2
Denis <ddujota@internationalsafety.com>
2021-07-01 07:27:50 -0500

    added dot env and a session helper for triggering it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def enable_logrocket?
+    in_production = (Rails.env.production? && ENV["DEPLOYMENT_MODE"] == "PRODUCTION")
+
+    enable = in_production || force_logrocket?
+
+    enable
+  end
+
+  def force_logrocket?
+    ENV["FORCE_LOGROCKET"] == "true"
+  end
 end
